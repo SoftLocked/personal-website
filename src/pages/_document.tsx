f433cae129b3e837db3640f1089865ebe2d7acf0
Harikrishna Vardhineedi <hvardhin@uci.edu>
2025-04-09 01:24:14 -0700

    Added vercel analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { Analytics } from "@vercel/analytics/react"
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,6 +9,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <Analytics/>
       </body>
     </Html>
   );
