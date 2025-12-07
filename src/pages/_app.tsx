c1f79ef93c4b4e419ee260e57a950418e3bb7bfe
Harikrishna <harivardhineedi@gmail.com>
2025-12-07 00:04:52 -0800

    Added title, description, and icon to website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import Head from 'next/head'
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -12,6 +14,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
         <Analytics/>
+        <Head>
+          <title>Harikrishna Vardhineedi | Quantum Computing Researcher</title>
+          
+          <link rel="icon" type="image/png" href="DarkLogo.png" />
+          <meta name="description" content="Researcher building structural principles for quantum computing." />
+        </Head>
         <Component {...pageProps} />
     </>
   );
